db49933c15be6f939eaee7ca89308ca29936a294
Joscelyn <JoscelynRose@gmail.com>
2019-09-09 17:08:53 -0400

    met MVP

--- server.js
+++ server.js
@@ -29,6 +29,68 @@ server.get('/api/accounts/:id', validateId, (req, res) => {
   res.status(200).json(req.account);
 });
 
+// add an account
+
+server.post('/api/accounts', (req, res) => {
+  const { name, budget } = req.body;
+
+  if (!name || typeof budget !== 'number') {
+    return res.status(400).json({
+      error: 'Needs name and budget. Budget must have a number value.'
+    });
+  }
+
+  db('accounts')
+    .insert({ name, budget }, 'id')
+    .then(id => {
+      db('accounts')
+        .where({ id }) // always return an array
+        .first() // picks the first element of the resulting array
+        .then(account => {
+          res.status(201).json(account);
+        });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// delete an account
+
+server.delete('/api/accounts/:id', validateId, (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json({ message: `deleted ${count} records` });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// edit an account
+
+server.put('/api/accounts/:id', validateId, (req, res) => {
+  const { name, budget } = req.body;
+
+  if (!name || typeof budget !== 'number') {
+    return res.status(400).json({
+      error: 'Needs name and budget. Budget must have a number value.'
+    });
+  }
+
+  db('accounts')
+    .where('id', req.params.id)
+    .update({ name, budget })
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 // middleware
 function validateId(req, res, next) {
   const { id } = req.params;
