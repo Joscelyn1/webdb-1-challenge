dc2b3b5a860f97d42ea097850146dc0c58935d79
Joscelyn <JoscelynRose@gmail.com>
2019-09-09 16:52:41 -0400

    finished get requests

--- server.js
+++ server.js
@@ -6,4 +6,45 @@ const server = express();
 
 server.use(express.json());
 
+// get home
+server.get('/', (req, res) => {
+  res.send('<h3>DB Helpers with knex</h3>');
+});
+
+// get accounts
+server.get('/api/accounts', (req, res) => {
+  db('accounts')
+    .select('*')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// get specific account
+server.get('/api/accounts/:id', validateId, (req, res) => {
+  const { id } = req.params;
+  res.status(200).json(req.account);
+});
+
+// middleware
+function validateId(req, res, next) {
+  const { id } = req.params;
+  db('accounts')
+    .where({ id })
+    .first()
+    .then(account => {
+      if (account) {
+        req.account = account;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({ error: 'There is no account with the specified id' });
+      }
+    });
+}
+
 module.exports = server;
